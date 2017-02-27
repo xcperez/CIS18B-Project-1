2a9d28e133111a819c54aa68d0471b901b11178f
Xavier Perez <Xavier Perez>
2017-02-26 22:14:56 -0800

    Completed Project 1

---
+++ Project1/src/Project1.java
@@ -0,0 +1,15 @@
+/*
+ * Xavier Perez 
+ * CIS 18B
+ * github user: xcperez
+ */
+
+
+public class Project1 {
+	
+	public static void main(String[]args){
+		
+		System.out.println("Hello World!");
+	}
+
+}
